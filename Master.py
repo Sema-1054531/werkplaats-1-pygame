367c3e89e25e7373b5229fe75389a5cb19590b2f
Thijs-1051036 <1051036@hr.nl>
2022-10-12 14:00:30 +0200

    ADD: Removed an unnecessary screen to make it look cleaner

--- Master.py
+++ Master.py
@@ -25,7 +25,7 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Main Menu")
 
 #game variables
-game_menu = False
+game_menu = True
 menu_state = "main"
 
 
@@ -37,8 +37,8 @@ start_img = pygame.image.load("../pythonProject2/Images/Play.png").convert_alpha
 quit_img = pygame.image.load("../pythonProject2/Images/Quit.png").convert_alpha()
 
 #Create button instances
-start_button = button.Button(screen_width * 0.33 - playbutton_width // 2, screen_height * 0.15, start_img, 0.75)
-quit_button = button.Button(screen_width * 0.66 - quitbutton_width // 2, screen_height * 0.15, quit_img, 0.75)
+start_button = button.Button(screen_width * 0.33 - playbutton_width // 2, screen_height * 0.3, start_img, 1)
+quit_button = button.Button(screen_width * 0.66 - quitbutton_width // 2, screen_height * 0.3, quit_img, 1)
 
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
@@ -52,6 +52,11 @@ while run:
 
     #check if game is in the menu
     if game_menu == True:
+        name_text_rect = name_text.get_rect(center=(screen_width // 2, screen_height // 7))
+        groupname_text_rect = groupname_text.get_rect(center=(screen_width * 0.9, screen_height // 1.05))
+
+        screen.blit(name_text, name_text_rect)
+        screen.blit(groupname_text, groupname_text_rect)
         #check menu state
         if menu_state == "main":
             #draw menu screen buttons
@@ -62,14 +67,7 @@ while run:
             if quit_button.draw(screen):
                 run = False
 
-    else:
-        name_text_rect = name_text.get_rect(center=(screen_width // 2, screen_height // 5))
-        startup_text_rect = startup_text.get_rect(center=(screen_width // 2, screen_height // 1.75))
-        groupname_text_rect = groupname_text.get_rect(center=(screen_width * 0.9, screen_height // 1.05))
 
-        screen.blit(startup_text, startup_text_rect)
-        screen.blit(name_text, name_text_rect)
-        screen.blit(groupname_text, groupname_text_rect)
 
 # event handler
     for event in pygame.event.get():
