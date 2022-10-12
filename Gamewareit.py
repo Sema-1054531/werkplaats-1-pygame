b8fac2527014eef25ebbe54c37a658666111a633
romius-1059571 <romiusboy@hotmail.com>
2022-10-12 14:31:36 +0200

    ADDED Kogel beweegt  en volgt vliegtuig

--- Gamewareit.py
+++ Gamewareit.py
@@ -1,7 +1,7 @@
 import random
 import pygame
 
-pygame.soundmixer(44100, 32, 16, 8)
+
 pygame.init()
 
 # create screen
@@ -29,9 +29,9 @@ enemyY_change = 40
 
 #Bullet
 bulletImg = pygame.image.load('bully.png')
-bulletX = 0
-bulletY = 480
-bulletX_change = 0
+bulletX = playerX
+bulletY = playerY
+bulletX_change = 3
 bulletY_change = 10
 bullet_state = "ready"
 bullet = []
@@ -44,7 +44,9 @@ def enemy(x,y):
     screen.blit(enemyImg, (x, y))
 
 def fire_bullet(x,y):
-    screen.blit(bulletImg,(x + 16, y + 10))
+    screen.blit(bulletImg,(x,y))
+
+
 
 #Game loop
 running = True
@@ -64,19 +66,23 @@ while running:
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_LEFT:
             playerX_change = -5
+            print("links")
         if event.key == pygame.K_RIGHT:
             playerX_change = 5
+            print("rechts")
         if event.key == pygame.K_DOWN:
-                playerY_change = 5
+            playerY_change = 5
+            print("omlaag")
         if event.key == pygame.K_UP:
-                playerY_change = -5
-
-        if event.key == pygame.K_SPACE:
-            print("bullshit")
+            playerY_change = -5
+            print("omhoog")
+        if event.key == pygame.K_LSHIFT:
             bulletX = playerX
+            bulletY = playerY
             fire_bullet(bulletX, bulletY)
-            while bullet_state == "ready":
-                bullet_state == "fire"
+            if bullet_state == "ready":
+                bullet_state = "fire"
+
 
     if event.type == pygame.KEYUP:
         if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -90,8 +96,12 @@ while running:
     playerY += playerY_change
     bulletX += bulletX_change
 
+
     player(playerX, playerY)
     enemy(enemyX, enemyY)
+    #bullet movement
+    if bullet_state == "fire":
+        fire_bullet(bulletX, bulletY)
     pygame.display.update()
 
 # checking for boundaries
@@ -112,10 +122,6 @@ while running:
         enemyX_change = -4
         enemyY += enemyY_change
 
-    #bullet movement
-    if bullet_state == "fire":
-        fire_bullet(playerX,bulletY)
-        bulletX += bulletX_change
 
 
 
